dacc548d8d08da56f6910fc1f87d070173a47ff2
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-27 14:15:55 +0900

    カスタムフィールドの内容を画面に表示
    get_post_meta
    
    if構文でのエラハンドリング

--- app/public/wp-content/themes/youma/template-parts/content-item.php
+++ app/public/wp-content/themes/youma/template-parts/content-item.php
@@ -1,2 +1,18 @@
 <p>商品名: <?php the_title(); ?></p>
+
+<?php
+$price = get_post_meta(get_the_ID(), '価格', true);
+$published = get_post_meta(get_the_ID(), '出版社', true);
+?>
+
+<dl>
+  <?php if ($price) : ?>
+    <dt>価格</dt>
+    <dd><?php echo esc_html(number_format($price)); ?>円</dd>
+  <?php endif; ?>
+  <?php if ($published) : ?>
+    <dt>出版社</dt>
+    <dd><?php echo esc_html($published); ?></dd>
+  <?php endif; ?>
+</dl>
 <p><?php the_content(); ?></p>
